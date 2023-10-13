af5e8c44d70b29c1f7d644d1d7569c532beee1c6
DM <druavhm@gmail.com>
2023-10-13 15:38:06 +0530

    Minor changes and improvements

--- app.js
+++ app.js
@@ -1,30 +1,27 @@
 let count = 0;
 let counter = document.getElementById("counter");
 let entries = document.getElementById("prevEnt");
-// let save = document.getElementById("save")
-
-// let welcome_el = document.getElementById("welcome_el")
-// let name = "Dhruv"
-// let greeting = "Welcome Back, "
-// let emoji = "👍👍"
-
-// welcome_el.innerText = greeting + name + emoji
 
 
 function inc() {
-    console.log("clicked");
     count += 1;
-    counter.innerText= count
-    console.log(count);
+    counter.textContent= count
+    console.log("Incremented by 1 : ",count);
  }
 
 function save(){
     let countStr = count + " - "
     entries.textContent += countStr    // entries.textContent = entries.textContent + count + "-"
-    console.log(count)
+    console.log("Data Saved : ", count)
+    count = 0;
+    counter.textContent= count
+    
+
 }
 //Reset Function
 function reset(){
     count = 0;
-    counter.innerText= count
+    counter.textContent= count
+    console.log("Counter Reset")
 }
+
